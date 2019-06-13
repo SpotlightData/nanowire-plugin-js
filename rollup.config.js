6a9a137de4aac9532cc78954271c5269a9ca0acc
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-13 14:45:16 +0100

    update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,18 @@
 import typescript from 'rollup-plugin-typescript';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 
 export default {
   input: './lib/index.ts',
-  plugins: [typescript()],
+  output: [
+    {
+      format: 'cjs',
+      file: 'dist/index.cjs.js',
+    },
+    {
+      format: 'esm',
+      file: 'dist/index.esm.js',
+    },
+  ],
+  plugins: [typescript(), resolve(), commonjs()],
 };
