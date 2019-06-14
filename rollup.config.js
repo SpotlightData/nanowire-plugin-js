24d70a0db6afec44cd9d54911a7cbf321c40372a
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-14 14:55:51 +0100

    Update build config

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import builtins from 'builtin-modules';
 
 export default {
   input: './lib/index.ts',
@@ -14,5 +16,15 @@ export default {
       file: 'dist/index.esm.js',
     },
   ],
-  plugins: [typescript(), resolve(), commonjs()],
+  externals: builtins,
+  plugins: [
+    json(),
+    typescript(),
+    resolve({ preferBuiltins: true }),
+    commonjs({
+      namedExports: {
+        'node_modules/lru_map/lru.js': ['LRUMap'],
+      },
+    }),
+  ],
 };
