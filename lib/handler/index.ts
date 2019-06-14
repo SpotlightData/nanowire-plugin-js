55123b3e9df29c2511f190d155a850ace22f3a1b
WhoAteDaCake <doitsimplefy@gmail.com>
2019-06-14 14:56:00 +0100

    Setup improved task retrieval

--- lib/handler/index.ts
+++ lib/handler/index.ts
@@ -1,25 +1,39 @@
 import * as Sentry from '@sentry/node';
-import { StringKV, getAllEnv } from '@lib/helpers';
+import axios from 'axios';
+import { StringKV, getAllEnv, sleep } from '@lib/helpers';
 
-export type Handler<M, A, U, R> = (meta: M, json: A, url: U) => R;
+export interface ControllerResponse<M, J> {
+  metadata: M;
+  jsonld: J;
+  url: string;
+}
+export type TaskHandler<M, J, R> = (data: ControllerResponse<M, J>) => R;
 
-export interface PluginHandlerParams<M, A, U, R> {
-  taskHandler: Handler<M, A, U, R>;
+export interface PluginHandlerParams<M, J, R> {
+  taskHandler: TaskHandler<M, J, R>;
   env: StringKV;
   name: string;
+  timeout: number;
 }
 
-export class PluginHandler<M, A, U, R> {
+export class PluginHandler<M, J, R> {
+  static LONGEST_TIMEOUT = 30 * 1000;
+
+  static exponentialTimeout(time: number) {
+    return Math.min(time * 1.5, PluginHandler.LONGEST_TIMEOUT);
+  }
+
   static formatUrl(base: string, pName: string, pId: string): string {
     const cleanBase = base.replace(/\/+$/, '');
     return `${cleanBase}/v1/tasks?pluginId=${pId}&pluginInstance=${pName}`;
   }
 
   controllerUrl: string;
-  taskHandler: Handler<M, A, U, R>;
+  timeout: number;
+  taskHandler: TaskHandler<M, J, R>;
 
-  constructor(params: PluginHandlerParams<M, A, U, R>) {
-    const { env, name, taskHandler } = params;
+  constructor(params: PluginHandlerParams<M, J, R>) {
+    const { env, name, taskHandler, timeout } = params;
 
     if (!params) {
       throw new TypeError('Missing parameters');
@@ -32,12 +46,42 @@ export class PluginHandler<M, A, U, R> {
     ]);
 
     this.taskHandler = taskHandler;
+    this.timeout = timeout;
     this.controllerUrl = PluginHandler.formatUrl(CONTROLLER_BASE_URI, POD_NAME, PLUGIN_ID);
 
     Sentry.init({
       dsn: env.SENTRY_DSN,
     });
+    Sentry.setTags({ pluginName: name });
   }
 
-  start() {}
+  getTask(): ControllerResponse<M, J> {
+    async function loop(timeout: number) {
+      await sleep(timeout);
+      try {
+        const resp = await axios.get(this.controllerUrl);
+        return resp.data;
+      } catch (e) {
+        // Controller not available use exponential back-off
+        if (e.code === 'ECONNREFUSED') {
+          return loop(PluginHandler.exponentialTimeout(timeout));
+        } else {
+          // Otherwise, just wait again
+          return loop(this.timeout);
+        }
+      }
+    }
+    return loop(0);
+  }
+
+  async singleFileLoop() {
+    const task = this.getTask();
+    try {
+    } catch (e) {}
+  }
+
+  start() {
+    // Only support single file atm
+    return this.singleFileLoop();
+  }
 }
